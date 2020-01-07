f5f5bb2587fe2250ceea6f1fcdf674911b65cd0f
Victor Manuel Canseco <aprog5@icave.com.mx>
2020-01-06 18:04:33 -0600

    .

---
+++ js/client-data-service.js
@@ -0,0 +1,42 @@
+"use strict";
+var MAX_TIMEOUT_SECONDS = 120 * 1000;
+const URL_CLIENT_MICROSERVICE = "http://localhost:6080/bhermanos-cobranza-api/api/clientes"
+
+function createClient(client) {
+
+    var url = URL_CLIENT_MICROSERVICE;
+
+    console.log("Submit URL " + url);
+    console.log(JSON.stringify(client));
+
+    var settings = {
+        "crossDomain": true,
+        "url": url,
+        "method": "POST",
+        "data": JSON.stringify(client),
+        "contentType": "application/json; charset=utf-8",
+        "dataType": "json",
+        "async": false,
+        "timeout": MAX_TIMEOUT_SECONDS
+    }
+
+    var jqXHR = $.ajax(settings);
+
+    return jqXHR;
+}
+
+function checkINE(ine) {
+    var url = URL_CLIENT_MICROSERVICE + "/exists?ine=" + ine;
+
+    var settings = {
+        "crossDomain": true,
+        "url": url,
+        "method": "GET",
+        "async": false,
+        "timeout": MAX_TIMEOUT_SECONDS
+    }
+
+    var jqXHR = $.ajax(settings);
+
+    return jqXHR;
+};
--- js/utils.js
+++ js/utils.js
@@ -2,3 +2,41 @@ function emailFormatter(value) {
 
     return '<a href="mailto:' + value + '"><u>' + value + '</u></a>';
 }
+
+function serializeForm(formId) {
+    var o = {};
+    var a = $(formId).serializeArray();
+    $.each(a, function () {
+        var componentName = this.name;
+        console.log("component_name " + componentName);
+        if (this.value.trim() !== "") {
+            if (o[componentName]) {
+                if (!o[componentName].push) {
+                    o[componentName] = [o[componentName]];
+                }
+                o[componentName].push(this.value || '');
+            } else {
+                o[componentName] = this.value || '';
+            }
+        }
+
+    });
+    return o;
+};
+
+
+function showAlert(title, message, alert) {
+    swal({
+        title: title,
+        text: message,
+        icon: alert,
+    });
+}
+
+function showAlertAsHTML(title, htmlmessage, alert) {
+    swal({
+        title: title,
+        content: htmlmessage,
+        icon: alert,
+    });
+}
