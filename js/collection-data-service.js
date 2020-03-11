1df13ba469194087227daf21ad569373519c9dcb
Victor Manuel Canseco <aprog5@icave.com.mx>
2020-03-11 17:04:55 -0600

    .

--- js/collection-data-service.js
+++ js/collection-data-service.js
@@ -17,4 +17,27 @@ function findPendingPaymentsByClient(date, clientId, includeOldPayments) {
     var jqXHR = $.ajax(settings);
 
     return jqXHR;
-};
+}
+
+function applyCollectionPayment(payment) {
+
+    var url = URL_COLLECTION_MICROSERVICE+"/aplicar-pago";
+
+    console.log("Submit URL " + url);
+    console.log(JSON.stringify(payment));
+
+    var settings = {
+        "crossDomain": true,
+        "url": url,
+        "method": "POST",
+        "data": JSON.stringify(payment),
+        "contentType": "application/json; charset=utf-8",
+        "dataType": "json",
+        "async": false,
+        "timeout": MAX_TIMEOUT_SECONDS
+    }
+
+    var jqXHR = $.ajax(settings);
+
+    return jqXHR;
+}
